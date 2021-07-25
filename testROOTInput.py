83079924582dbcee8ce7734fabe0bb40016dd266
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2021-07-25 23:13:46 +0100

    add examples with ROOT input

---
+++ testROOTInput.py
@@ -0,0 +1,99 @@
+import ROOT
+import numpy as np
+import scipy.stats
+
+
+def convertToArray(filename):
+    '''convert branch from Kamal's christmas file
+    to numpy'''
+    rootFile = ROOT.TFile.Open(filename)
+    tree = rootFile.Get("subtree")
+    list_tmp = []
+    for event in tree:
+        v = event.sigma_qp
+        for i in v:
+            list_tmp.append(i)
+    tmp = np.array(list_tmp)
+    sort_tmp = np.sort(tmp)
+    low = np.percentile(sort_tmp, 0.5)
+    up = np.percentile(sort_tmp, 99.5)
+    return sort_tmp[(sort_tmp > low) & (sort_tmp < up)]
+
+
+def applyDistortion(inputArray):
+    ''' Add a normal to each entry '''
+    distortion = scipy.stats.norm(loc=0.001, scale=0.0015)
+    return inputArray + distortion.rvs(size=len(inputArray))
+
+
+def correction(inputMC, pseudoData):
+
+    # linear scaling
+    mean_data = np.mean(pseudoData)
+    mean_simul = np.mean(inputMC)
+    estShift = mean_data - mean_simul
+    # up to here we correct just a shift
+    LSCorrected = inputMC + estShift
+
+    # Let's perform a simplified smearing
+    # shift everything to 0 mean
+    mean_shifted_simul = np.mean(LSCorrected)
+    zero_mean_simul = LSCorrected - mean_shifted_simul
+    # And then calculate the ratio of the data simul sigma
+    sigma_data = np.std(pseudoData)
+    sigma_simul = np.std(zero_mean_simul)
+    sigma_ratio = sigma_data/sigma_simul
+    # The final corrected one
+    corrected = zero_mean_simul * sigma_ratio + mean_shifted_simul
+
+    # check that the 2 first moments are now close
+    print("original MC mean ", np.mean(inputMC),
+          " pseudodata mean ", np.mean(pseudoData),
+          " corrected mean", np.mean(corrected))
+    print("original MC sigma ", np.std(inputMC),
+          " pseudodata sigma ", np.std(pseudoData),
+          " corrected sigma", np.std(corrected))
+
+    # plotting things ...
+    # pick some reasonable min and max using
+    # the input entries
+    hMax = max(max(inputMC), max(pseudoData))
+    hMin = min(min(inputMC), min(pseudoData))
+
+    hMC = ROOT.TH1F("hMC", "hMC", 100, hMin, hMax)
+    hPseudoData = ROOT.TH1F("hPseudoData", "hPseudoData", 100, hMin, hMax)
+    hLSCorrected = ROOT.TH1F("hLSCorrected", "hLSCorrected", 100, hMin, hMax)
+    hVARICorrected = ROOT.TH1F(
+        "hVARICorrected", "hVARICorrected", 100, hMin, hMax)
+    c1 = ROOT.TCanvas("c1", "c1")
+    c1.cd()
+
+    for i in np.nditer(inputMC):
+        hMC.Fill(i)
+
+    for i in np.nditer(pseudoData):
+        hPseudoData.Fill(i)
+
+    for i in np.nditer(LSCorrected):
+        hLSCorrected.Fill(i)
+
+    for i in np.nditer(corrected):
+        hVARICorrected.Fill(i)
+    # colors ....
+    hMC.SetLineColor(ROOT.kRed+1)
+    hPseudoData.SetLineColor(ROOT.kBlack)
+    hLSCorrected.SetLineColor(ROOT.kBlue+1)
+    hVARICorrected.SetLineColor(ROOT.kSpring-6)
+    hMC.Draw()
+    hPseudoData.Draw("SAME")
+    # hLSCorrected.Draw("SAME")
+    hVARICorrected.Draw("SAME")
+
+    c1.SaveAs("testVARI.pdf")
+
+
+if __name__ == "__main__":
+    MC = convertToArray("MC_eta0.4_0.8_pt30_45_psec5_CB.root")
+    print("entries", len(MC))
+    pseudoData = applyDistortion(MC)
+    correction(MC, pseudoData)
---
+++ testRooFitConvole.py
@@ -0,0 +1,93 @@
+import ROOT
+import numpy as np
+import scipy.stats
+import math
+
+
+def convertToArray(filename):
+    '''convert branch from Kamal's christmas file
+    to numpy'''
+    rootFile = ROOT.TFile.Open(filename)
+    tree = rootFile.Get("subtree")
+    list_tmp = []
+    for event in tree:
+        v = event.sigma_qp
+        for i in v:
+            list_tmp.append(i)
+    tmp = np.array(list_tmp)
+    sort_tmp = np.sort(tmp)
+    low = np.percentile(sort_tmp, 0.5)
+    up = np.percentile(sort_tmp, 99.5)
+    return sort_tmp[(sort_tmp > low) & (sort_tmp < up)]
+
+
+def applyDistortion(inputArray):
+    ''' Add a normal to each entry '''
+    distortpdf = scipy.stats.norm(loc=0.001, scale=0.0015)
+    distortion = distortpdf.rvs(size=len(inputArray))
+    return distortion, inputArray + distortion
+
+
+def correction(inputMC, pseudoData, distortion):
+
+    hMax = max(max(pseudoData), max(inputMC))
+    hMin = min(min(pseudoData), min(inputMC))
+    numBins = 100
+    hMC = ROOT.TH1F("hMC", "hMC", numBins, hMin, hMax)
+    hPseudoData = ROOT.TH1F("hPseudoData", "hPseudoData", numBins, hMin, hMax)
+    # hPseudoData.Sumw2()
+    for i in np.nditer(inputMC):
+        hMC.Fill(i)
+    for i in np.nditer(pseudoData):
+        hPseudoData.Fill(i)
+
+    # set up a full convolution model
+    # Let's get some initial Guesses for the mean
+    # and the sigma of the Gauss
+    estShift = hPseudoData.GetMean() - hMC.GetMean()
+    sigmaMC = hMC.GetRMS()
+    sigmaData = hPseudoData.GetRMS()
+    estSmear = math.sqrt(sigmaData*sigmaData-sigmaMC*sigmaMC)
+    # variable for the observable
+    x = ROOT.RooRealVar("x", "x", hMin, hMax)
+    # data (what we want to fit to)
+    data = ROOT.RooDataHist("data", "data", x, hPseudoData)
+    # create an probability denisty function
+    # for the MC
+    hRooMC = ROOT.RooDataHist("hRooMC", "hRooMC", x, hMC)
+    hpdfMC = ROOT.RooHistPdf("hpdfMC", "hpdfMC", x, hRooMC, 0)
+
+    # A Gaussian we convolve with
+    meanG = ROOT.RooRealVar("meanG", "meanG", estShift,
+                            0.5*estShift, 1.5*estShift)
+    sigmaG = ROOT.RooRealVar(
+        "sigmaG", "sigmaG",
+        estSmear, 0.0, 1.5 * estSmear)
+    gauss = ROOT.RooGaussian("gauss", "gauss", x, meanG, sigmaG)
+
+    # The actual convolution
+    histXGaus = ROOT.RooFFTConvPdf(
+        "histXGaus", "histo (X) gauss", x, hpdfMC, gauss)
+    histXGaus.fitTo(data)
+    print("fitted mean", meanG.getVal())
+    print("fitted sigma ", sigmaG.getVal())
+
+    # Some plotting ...
+    c1 = ROOT.TCanvas("c1", "c1")
+    c1.cd()
+    xframe = x.frame()
+    data.plotOn(xframe)
+    hRooMC.plotOn(xframe,
+                  ROOT.RooFit.DrawOption("C"),
+                  ROOT.RooFit.LineColor(ROOT.kRed))
+    histXGaus.plotOn(xframe,
+                     ROOT.RooFit.LineColor(ROOT.kSpring-6))
+    xframe.Draw()
+    c1.SaveAs("testFit.pdf")
+
+
+if __name__ == "__main__":
+    MC = convertToArray("MC_eta0.4_0.8_pt30_45_psec5_CB.root")
+    print("entries", len(MC))
+    distortion, pseudoData = applyDistortion(MC)
+    correction(MC, pseudoData, distortion)
